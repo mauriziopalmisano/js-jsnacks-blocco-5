b9ea7194ca0d7d7191a83b5a240156c2bc476844
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-14 15:14:09 +0200

    snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,13 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+const names = people.map((person, index) => person.name)
+let listaNomi = '';
+names.forEach((name, index) =>{
+    listaNomi += `'${name}', `;
+} )
+console.log(listaNomi.trim());
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
