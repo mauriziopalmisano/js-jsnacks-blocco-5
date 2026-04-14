dec447bd95646344f3324a37f681cf6bd1b2394b
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-14 15:22:24 +0200

    snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -4,6 +4,13 @@ const posts = [
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
+console.log(posts);
+
+
+
+const authors = posts.map((post,index) => post.author);
+console.log(authors);
+
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
