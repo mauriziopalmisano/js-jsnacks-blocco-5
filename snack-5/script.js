cf24307e22fd91b1a8bf2820069ab8e6dd093ecf
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-14 15:24:49 +0200

    snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,9 @@
 const nums = [2, 8, 4, 7, 12, 87];
+console.log(nums);
+
+
+const numsEven = nums.filter((num, index) => num % 2 === 0);
+console.log(numsEven);
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
