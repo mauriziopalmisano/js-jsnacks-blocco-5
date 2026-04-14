00727198d2b49bdc0be0a434d05e43854f58ab1f
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-14 15:09:17 +0200

    snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-
+let listaNomi = '';
+names.forEach((name, index) =>{
+    listaNomi += `'${name}', `;
+} )
+console.log(listaNomi.trim());
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
